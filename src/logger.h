51166df783ab8048cb4682fac9b8f208282ff359
Jacob Lewallen <jlewallen@gmail.com>
2018-07-13 11:11:59 -0700

    Add SimpleLog template.

--- src/logger.h
+++ src/logger.h
@@ -98,4 +98,45 @@ public:
 
 };
 
+// NOTE: This is a C++11 feature.
+template<const char *Name>
+class SimpleLog {
+public:
+    static void log(const char *f, ...) {
+        va_list args;
+        va_start(args, f);
+        vlogf(LogLevels::INFO, Name, f, args);
+        va_end(args);
+    }
+
+    static void info(const char *f, ...) {
+        va_list args;
+        va_start(args, f);
+        vlogf(LogLevels::INFO, Name, f, args);
+        va_end(args);
+    }
+
+    static void trace(const char *f, ...) {
+        va_list args;
+        va_start(args, f);
+        vlogf(LogLevels::TRACE, Name, f, args);
+        va_end(args);
+    }
+
+    static void warn(const char *f, ...) {
+        va_list args;
+        va_start(args, f);
+        vlogf(LogLevels::WARN, Name, f, args);
+        va_end(args);
+    }
+
+    static void error(const char *f, ...) {
+        va_list args;
+        va_start(args, f);
+        vlogf(LogLevels::ERROR, Name, f, args);
+        va_end(args);
+    }
+
+};
+
 #endif
